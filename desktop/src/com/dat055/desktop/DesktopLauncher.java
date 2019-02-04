9a18edab090a141d128eaf870010b3d0ce4dd045
Karl Strålman <kallestralman@gmail.com>
2019-02-04 14:16:18 +0100

    Added generated files from libGDX

---
+++ desktop/src/com/dat055/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dat055.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dat055.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
