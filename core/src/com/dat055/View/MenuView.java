2d918ecff295d273757a1db4775d1eb30a32d1bf
arktisZ10 <erik.borne@outlook.com>
2019-02-06 02:36:11 +0100

    + Lade till uiskins (.atlas/.json/.png) wip
    * Små ändringar till MenuView
    * Bytte namn på MenuScreen till MainScreen

--- core/src/com/dat055/View/MenuView.java
+++ core/src/com/dat055/View/MenuView.java
@@ -3,21 +3,22 @@ package com.dat055.View;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.freetype.*;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.dat055.Model.MenuModel;
-import com.dat055.View.Screen.MenuScreen;
+import com.dat055.View.Screen.MainScreen;
 import com.dat055.View.Screen.Screen;
 
 import java.util.ArrayList;
 
 public class MenuView extends View{
     private static MenuView instance = null;
-    private MenuScreen menuScreen;
+    private MainScreen menuScreen;
 
     private static BitmapFont font;
 
-
+    /**
+     *
+     */
     private MenuView() { screens = new ArrayList<Screen>(); }
     private static void initFonts() {
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/Cabin-SemiBold.ttf"));
@@ -30,6 +31,10 @@ public class MenuView extends View{
         font = generator.generateFont(params);
     }
 
+    /**
+     * A singleton object that handles menu screens.
+     * @return the instance that exist or has been created.
+     */
     public static synchronized MenuView getInstance() {
         if ( instance == null ) {
             instance = new MenuView();
@@ -37,11 +42,24 @@ public class MenuView extends View{
         } return instance;
     }
 
+    /**
+     * A simple get function
+     * @return the general font.
+     */
+    public BitmapFont getFont() { return font; }
+
+    /**
+     *
+     */
     @Override
     public void update() {
         for(Screen screen : screens) {screen.update();}
     }
 
+    /**
+     *
+     * @param batch
+     */
     @Override
     public void draw(SpriteBatch batch) {
         for(Screen screen : screens) {screen.draw(batch);}
--- core/src/com/dat055/View/Screen/MainScreen.java
+++ core/src/com/dat055/View/Screen/MainScreen.java
@@ -4,9 +4,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.dat055.Model.GameModel;
 import com.dat055.Model.MenuModel;
 
-public class MenuScreen implements Screen {
+public class MainScreen implements Screen {
     MenuModel menuModel; // logic for game stored here
-    public MenuScreen(String fileName) {
+    public MainScreen(String fileName) {
 
     }
 
