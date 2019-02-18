8bb1a779a1583564e531ed45fb44e4d9224bd8fd
Katla <whiaen@hotmail.com>
2019-02-18 19:33:18 +0100

    + Now with textures! (should work)

---
+++ core/src/com/dat055/model/entity/Interactable.java
@@ -0,0 +1,19 @@
+package com.dat055.model.entity;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+
+public class Interactable extends Entity {
+
+    public Interactable(Vector2 position, int height, int width, String texturePath){
+        super(position, 128, 64, texturePath);
+    }
+    public void draw(SpriteBatch sb, float position){
+        super.draw(sb, position);
+    }
+    @Override
+    public void action(String act){
+
+    }
+}
