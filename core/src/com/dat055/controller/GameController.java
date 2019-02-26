b71530a722cf898d1c2dd835e771650c86f12652
Kjelle <kallestralman@gmail.com>
2019-02-26 01:15:19 +0100

    * Gave threads a chance to run

--- core/src/com/dat055/controller/GameController.java
+++ core/src/com/dat055/controller/GameController.java
@@ -215,7 +215,15 @@ public class GameController extends Controller {
 
         System.out.println("Map created");
         //TODO: Skapa loadingscreen-tråd
-        while(!server.isRunning()); // wait for map to be set
+
+        while(true) {
+            if(!server.isRunning()) {
+                try {
+                    Thread.sleep(0);
+                } catch (InterruptedException ignored) {}
+            } else { break; }
+        }
+
         //Host decides this from menu
         mode = Mode.FRONT;
         isMultiplayer = true;
@@ -232,7 +240,13 @@ public class GameController extends Controller {
         if(!server.startServerAndClient(addr))
             return false;
 
-        while(!server.isRunning()); // wait for map to be set
+        while(true) {
+            if(!server.isRunning()) {
+                try {
+                    Thread.sleep(0);
+                } catch (InterruptedException ignored) {}
+            } else { break; }
+        }
 
         System.out.println("Map choosen from server: " + server.getChosenMap());
         mode = Mode.BACK; //TODO: This will be set from message from other peer
