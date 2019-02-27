fb3c98a465b0f4d162cb373895c3015a50adc65b
Stoinkatron3000 <46894524+Stoinkatron3000@users.noreply.github.com>
2019-02-27 15:04:22 +0100

    + added text based input in settingsmenu

--- core/src/com/dat055/model/menu/SettingsMenu.java
+++ core/src/com/dat055/model/menu/SettingsMenu.java
@@ -4,66 +4,70 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.dat055.controller.MenuController;
-import com.dat055.model.menu.Menu;
-
-import java.util.ArrayList;
 
 public class SettingsMenu extends Menu {
     private MenuController controller;
-    private ArrayList<TextButton> list = new ArrayList();
-    private Image image;
     private TextButton video, audio, other, back;
     private Label resolution, fullscreen, graphics, volume;
+    private TextField resText, fulText, volText;
     public SettingsMenu(MenuController ctrl) {
         super("UI/Delta.jpg");
         initTxtBtnStyle();
         initLblStyle();
-
-        list.add(video);
-        list.add(audio);
-        list.add(other);
+        initTxtFldStyle(40);
 
         controller = ctrl;
         // Table table = new Table();
-        Table topTable = new Table();
+        Table table = new Table();
 
+        table.setWidth(controller.getWidth());
+        table.setHeight(controller.getHeight());
+        table.align(Align.center | Align.top);
 
-        topTable.setWidth(controller.getWidth());
-        topTable.setHeight(controller.getHeight());
-        topTable.align(Align.center | Align.top);
+        table.setPosition(0, 0);
 
-        topTable.setPosition(0, 0);
-
+        //Buttons
         video = createButton("Video");
         audio = createButton("Audio");
         other = createButton("Other");
         back = createButton("Back");
+        resText = createTextField("1024x768");
+        fulText = createTextField("0");
+        volText = createTextField("1");
 
+        //Labels
         resolution = new Label("Resolution",lblStyle);
         fullscreen = new Label("FullScreen",lblStyle);
         volume = new Label("Volume",lblStyle);
 
 
+
         addListeners();
 
-        topTable.add(back).width(150).height(40);
-        topTable.add(video).width(170).height(40);
-        topTable.add(audio).width(170).height(40);
-        topTable.add(other).width(170).height(40).row();
-        topTable.add();
-        topTable.add(resolution).padTop(20).height(40).padBottom(20).row();
-        topTable.add();
-        topTable.add(fullscreen).height(40).padBottom(20).row();
-        topTable.add();
-        topTable.add(volume).height(40);
+        table.debug();
+        table.add(back).width(150).height(40);
+        table.add(video).width(170).height(40);
+        table.add(audio).width(170).height(40);
+        table.add(other).width(170).height(40).row();
+        table.add();
+        table.add(resolution).padTop(20).height(40).padBottom(10);
+        table.add(resText).padTop(20).padBottom(10).row();
+        table.add();
+        table.add(fullscreen).height(40).padBottom(20).padTop(10);
+        table.add(fulText).padTop(10).padBottom(20).row();
+        table.add();
+        table.add(volume).height(40);
+        table.add(volText);
 
-        super.table = topTable;
+        super.table = table;
     }
 
     private void initTxtBtnStyle() {
@@ -86,16 +90,27 @@ public class SettingsMenu extends Menu {
         super.txtBtnStyle = txtBtnStyle;
     }
 
-    private void initTxtFldStyle() {
+    private void initTxtFldStyle(int height) {
         TextField.TextFieldStyle txtFldStyle = new TextField.TextFieldStyle();
-        BitmapFont font = generateFont(30);
 
-        txtFldStyle.font = font;
-        txtFldStyle.disabledFontColor = Color.WHITE;
-        txtFldStyle.messageFont = generateFont(20);
+        Skin skin = new Skin(Gdx.files.internal("UI/ui.json"));
+        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("UI/ui.atlas"));
+        skin.addRegions(atlas);
+
+        txtFldStyle.font = fontPad(generateFont(height-Gdx.graphics.getHeight()/50));
+        txtFldStyle.background = skin.getDrawable("but1");
+        txtFldStyle.fontColor = Color.WHITE;
+        txtFldStyle.messageFont = generateFont(height-Gdx.graphics.getHeight()/60);
         txtFldStyle.messageFontColor = Color.RED;
+        txtFldStyle.cursor = new NinePatchDrawable(new NinePatch(new Texture("UI/cursor.9.png")));
 
         super.txtFldStyle = txtFldStyle;
+    }
+
+    private BitmapFont fontPad(BitmapFont f) {
+        BitmapFont.BitmapFontData fd = f.getData();
+        fd.padLeft = -5;
+        return f;
     }
 
     private void initLblStyle() {
