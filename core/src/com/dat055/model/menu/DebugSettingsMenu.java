fae5768954ba7307dc8f1cde609c87a15bb9a671
Towbie <karatias@hotmail.com>
2019-03-11 14:32:55 +0100

    + Sound effects (new and for more actions!) * Change spike damage detection, doesn't kill player with i-frames.

--- core/src/com/dat055/model/menu/DebugSettingsMenu.java
+++ core/src/com/dat055/model/menu/DebugSettingsMenu.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.dat055.controller.MenuController;
-import sun.tools.jar.Main;
 
 import java.io.IOException;
 import java.util.ArrayList;
