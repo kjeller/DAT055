15718ad85102161b11a78fa1d20cf6c53e4f9b4b
Kalle Strålman <kallestralman@gmail.com>
2023-12-27 23:36:43 +0100

    New proj init

---
+++ desktop/src/com/mygdx/game/DesktopLauncher.kt
@@ -0,0 +1,15 @@
+package com.mygdx.game
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = Lwjgl3ApplicationConfiguration()
+        config.setForegroundFPS(60)
+        config.setTitle("My GDX Game")
+        Lwjgl3Application(MyGdxGame(), config)
+    }
+}
